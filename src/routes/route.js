0674a04a754921e2bdad99886d9cb5560cbd0bbd
monika <bansalmonika143@gmail.com>
2022-08-07 12:27:22 +0530

    changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
+const logger = require("../logger/logger")
+const helper = require("../utill/helper")
+const formatter = require("../helper/formatter")
+
 
 router.get('/test-me', function (req, res) {
     console.log('My batch is', abc.name)
     abc.printName()
+    logger.welcome()
+
+    helper.printDate()
+    helper.printMonth()
+    helper.printBatch()
+
+    let x = "function up"
+    x = formatter.trim(x)
+    formatter.changeToUpperCase()
+    formatter.changeToLowerCase()
+
     res.send('My second ever api!')
 });
 
---
+++ src/utill/helper.js
@@ -0,0 +1,14 @@
+let date = new date()
+
+exports.printdate()= function() {
+    console.log("current date :"+date.getDate())
+}
+
+exports.printmonth() = function(){
+    console.log("current month :" +date.getMonth())
+}
+
+exports.printBatch() = function(){
+    console.log("plutonium,W3D3, the topic for today is nodejs module system")
+}
+
