8fd23002cc27ef653a70471231801308de67a8b1
laptop-mb <bansalmonika143@gmail.com>
2022-08-11 21:01:27 +0530

    bar bar message mt pucha kro yr

--- src/routes/route.js
+++ src/routes/route.js
@@ -23,7 +23,6 @@ router.get('/student-details/:name', function(req, res){
     object where key is the variable defined in code 
     and value is what is sent in the request
     */
-
     let requestParams = req.params
 
     // JSON strigify function helps to print an entire object
@@ -35,26 +34,43 @@ router.get('/student-details/:name', function(req, res){
     res.send('Dummy response')
 })
 
+// 1.	Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
+router.get("/movies", function(req,res){
+ let moviesArray = ["3 iditos", "titanic","pk", "forest gump"]
+    res.send(moviesArray)
+})
+
+// 2.	Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1). You can define an array of movies again in your api
 router.get("/movies/:indexNumber", function(req,res){
-    let requestParams = req.params.indexNumber
-
-    let moviesArray = ["3 iditos", "titanic","pk", "forest gump"]
-    // console.log(moviesArray)
-    res.send(moviesArray[requestParams])
+//line no. 39 pr array
+let index = req.params
+console.log(movies[index.indexNumber])
+res.send(movies[index.indexNumber])
 })
 
-// router.get("/movies/:indexNumber", function(req,res){
-//     console.log("this request is "+JSON.stringify(req.params))
-//     console.log("indexNumber is", " " + req.params.indexNumber)
-//     res.send("done")
-// })
+// 3.	Handle a scenario in problem 2 where if the index is greater than the valid maximum value a message is returned that tells the user to use a valid index in an error message.
+router.get("/moviesHand/:indexNumber", function(req,res){
 
-router.get("/shoes",function(req,res){
-    let shoesQuery = req.query.shoes
-    console.log(shoesQuery)
 })
 
-router.get("/films/:filmId" , function(req,res){
+// 4.	Write another api called GET /films. Instead of an array of strings define an array of movie objects this time. Each movie object should have values - id, name. An example of movies array is 
+// [ {
+//  “id”: 1,
+//  “name”: “The Shining”
+// }, {
+//  “id”: 2,
+//  “name”: “Incendies”
+// }, {
+//  “id”: 3,
+//  “name”: “Rang de Basanti”
+// }, {
+//  “id”: 4,
+//  “name”: “Finding Nemo”
+// }]
+
+// Return the entire array in this api’s response
+
+router.get("/films" , function(req,res){
     let moviesArray2 = [ {
         "id": 1,
         "name": "The Shining"
@@ -69,18 +85,17 @@ router.get("/films/:filmId" , function(req,res){
         "name": "Finding Nemo"
        }]
 
-       let filmId = req.params.filmId
-       for(let i=0; i<films.length; i++){
-        let film = films[i]
-        if(film.id==filmId){
-            return res.send(film)
-        }
-       }
-       res.send("the film id dosent match any movie")
+       res.send(moviesArray2)         
+})
 
-       console.log(moviesArray2)
-       res.send(moviesArray2)
-          
+// 5.	Write api GET /films/:filmId where filmId is the value received in request path params. Use this value to return a movie object with this id. In case there is no such movie present in the array, return a suitable message in the response body. Example for a request GET /films/3 should return the movie object 
+// {
+//  “id”: 3,
+//  “name”: “Rang de Basanti”
+// }
+// Similarly for a request GET /films/9 the response can be something like - ‘No movie exists with this id’
+router.get("/films/:filmId", function(req,res){
+
 })
 
 module.exports = router;
