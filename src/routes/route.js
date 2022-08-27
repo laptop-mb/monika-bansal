242d3815a263941d4a202f4ad5a3d097241a71e2
laptop-mb <bansalmonika143@gmail.com>
2022-08-27 23:48:35 +0530

    correct the previous code

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,33 @@
 const express = require('express');
-const abc = require('../introduction/intro')
 const router = express.Router();
+const lodash =  require("lodash")
 
 router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
+    
+let monthsName = ['jan','feb','march','april','may','june','july','aug','oct','nov','dec']
+console.log(lodash.chunk(monthsName, 3))
+
+//================================================================//
+
+let x = [1,3,5,7,9,11,13,15,17,19]
+let oddNumberArray = lodash.tail(x)
+console.log(oddNumberArray)
+
+//==========================================================//
+
+let array = ([1,2,2,], [2,3,3] ,[3,4,4], [4,5,5] ,[5,6,6])
+let mergedArray = lodash.union(array)
+console.log(mergedArray)
+
+//===============================================================//
+
+let  array2 = [ ["horror","the shinning"], ["drama","tatanic"], ["thrillar","shutter island"], ["fantasy", "pans labringth"]]
+let objectArray = lodash.fromPairs(array2)
+console.log(objectArray)
+
+//================================================//
     res.send('My second ever api!')
 });
 
 
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-})
-
-router.get('/give-me-students-data',function(req, res){
-
-})
 module.exports = router;
-// adding this comment for no reason
